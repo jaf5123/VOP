a898e1099a435b3a16d8c0d74e8ac2e7a8c3ecd1
abdelilahelhachiouiBZ <abdelilah.elhachioui@bazandpoort.be>
2023-11-21 12:00:39 +0100

    update main

---
+++ src/main/java/Persoon.java
@@ -0,0 +1,66 @@
+import java.time.LocalDate;
+
+public class Persoon {
+    private String naam, achternaam, adres;
+    private int leeftijd;
+    private LocalDate geboortedatum;
+
+    public Persoon(String naam, String achternaam, int leeftijd, String adres, LocalDate geboortedatum) {
+        this.naam = naam;
+        this.achternaam = achternaam;
+        this.leeftijd = leeftijd;
+        this.adres = adres;
+        this.geboortedatum = geboortedatum;
+    };
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setLeeftijd(int leeftijd) {
+        this.leeftijd = leeftijd;
+    }
+
+    public int getLeeftijd() {
+        return leeftijd;
+    }
+
+    public void setGeboortedatum(LocalDate geboortedatum) {
+        this.geboortedatum = geboortedatum;
+    }
+
+    public LocalDate getGeboortedatum() {
+        return geboortedatum;
+    }
+
+    @Override
+    public String toString() {
+        String tekst = new String();
+
+        tekst += "*************** Persoon info ***************\n";
+        tekst += "Naam: " + getNaam() + ", achternaam: " + getAchternaam() + ", leeftijd: " + getLeeftijd() + "\n";
+        tekst += "Woont op " + getAdres() + ", en is geboren op " + getGeboortedatum() + "\n";
+
+        return tekst;
+    }
+}
--- src/main/java/Tennisclub.java
+++ src/main/java/Tennisclub.java
@@ -1,3 +1,11 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Tennisclub {
-    
-}
+    private String clubmanager, clubnaam, locatie, telefoonnummer;
+    private LocalTime openingsuur, sluitingsuur;
+/*    private ArrayList<Speler> spelers;
+    private ArrayList<Tennisbaan> tennisbanen;
+    private ArrayList<Trainer> trainers;
+    private ArrayList<Training> trainingsmomenten;*/
+};
