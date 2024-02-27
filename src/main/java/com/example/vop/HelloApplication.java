2dd3f87d290144391fc308a9312d4eb842fcfa7a
abdelilahelhachiouiBZ <abdelilah.elhachioui@bazandpoort.be>
2024-02-27 11:58:10 +0100

    update main
    >:)

--- src/main/java/com/example/vop/HelloApplication.java
+++ src/main/java/com/example/vop/HelloApplication.java
@@ -1,19 +1,35 @@
 package com.example.vop;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+//JavaFX
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.*;
+import javafx.geometry.*;
 
-import java.io.IOException;
+//Standard IO
+import java.io.*;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
+        Scene m_scene = null;
+        VBox m_root = new VBox(0);
+        m_scene = new Scene(m_root, 960, 650);
 
-    }
+        //Get the width and height of the window.
+        Rectangle2D m_renderableBox = Screen.getPrimary().getBounds();
+        final double m_width = m_renderableBox.getWidth();
+        final double m_height = m_renderableBox.getHeight();
+
+        primaryStage.setScene(m_scene);
+        primaryStage.setTitle("De Mol - tennisclub, gelegen in Duffel");
+        primaryStage.show();
+    };
 
     public static void main(String[] args) {
         launch();
-    }
+    };
 }
